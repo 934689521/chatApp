c1818192c9c32c3bb4bd848167b43e6b6c03c8d2
934689521 <934689521@qq.com>
2017-04-12 23:17:43 +0800

    .

--- app/src/androidTest/java/chat/app/com/chatapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/chat/app/com/chatapp/ExampleInstrumentedTest.java
@@ -20,7 +20,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
+//12
         assertEquals("chat.app.com.chatapp", appContext.getPackageName());
     }
 }
